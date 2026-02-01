0e9cd2bc30bf4891b50c4e0894fcb7fb34ee95a7
Hierugu <ilya.abramov.2002@inbox.ru>
2026-02-01 11:18:01 +0300

    feat: HW2 base done

--- 2/solution.cpp
+++ 2/solution.cpp
@@ -2,44 +2,303 @@
 
 #include <assert.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <unistd.h>
+#include <sys/wait.h>
+#include <fcntl.h>
+#include <string.h>
+#include <errno.h>
+#include <vector>
 
-static void
-execute_command_line(const struct command_line *line)
+static int
+execute_single_command(const struct command *cmd, int input_fd, int output_fd)
 {
-	/* REPLACE THIS CODE WITH ACTUAL COMMAND EXECUTION */
-
-	assert(line != NULL);
-	printf("================================\n");
-	printf("Command line:\n");
-	printf("Is background: %d\n", (int)line->is_background);
-	printf("Output: ");
-	if (line->out_type == OUTPUT_TYPE_STDOUT) {
-		printf("stdout\n");
-	} else if (line->out_type == OUTPUT_TYPE_FILE_NEW) {
-		printf("new file - \"%s\"\n", line->out_file.c_str());
-	} else if (line->out_type == OUTPUT_TYPE_FILE_APPEND) {
-		printf("append file - \"%s\"\n", line->out_file.c_str());
+	pid_t pid = fork();
+	if (pid == 0) {
+		if (input_fd != STDIN_FILENO) {
+			dup2(input_fd, STDIN_FILENO);
+			close(input_fd);
+		}
+		if (output_fd != STDOUT_FILENO) {
+			dup2(output_fd, STDOUT_FILENO);
+			close(output_fd);
+		}
+		
+		char **argv = new char*[cmd->args.size() + 2];
+		argv[0] = strdup(cmd->exe.c_str());
+		for (size_t i = 0; i < cmd->args.size(); ++i) {
+			argv[i + 1] = strdup(cmd->args[i].c_str());
+		}
+		argv[cmd->args.size() + 1] = nullptr;
+		
+		execvp(cmd->exe.c_str(), argv);
+		
+		perror("execvp");
+		
+		for (size_t i = 0; i <= cmd->args.size(); ++i) {
+			free(argv[i]);
+		}
+		delete[] argv;
+		
+		exit(EXIT_FAILURE);
+	} else if (pid > 0) {
+		if (input_fd != STDIN_FILENO) {
+			close(input_fd);
+		}
+		if (output_fd != STDOUT_FILENO) {
+			close(output_fd);
+		}
+		
+		int status;
+		waitpid(pid, &status, 0);
+		return WIFEXITED(status) ? WEXITSTATUS(status) : 1;
 	} else {
-		assert(false);
+		perror("fork");
+		return 1;
 	}
-	printf("Expressions:\n");
-	for (const expr &e : line->exprs) {
-		if (e.type == EXPR_TYPE_COMMAND) {
-			printf("\tCommand: %s", e.cmd->exe.c_str());
-			for (const std::string& arg : e.cmd->args)
-				printf(" %s", arg.c_str());
-			printf("\n");
-		} else if (e.type == EXPR_TYPE_PIPE) {
-			printf("\tPIPE\n");
-		} else if (e.type == EXPR_TYPE_AND) {
-			printf("\tAND\n");
-		} else if (e.type == EXPR_TYPE_OR) {
-			printf("\tOR\n");
-		} else {
-			assert(false);
+}
+
+static int
+execute_cd_command(const struct command *cmd)
+{
+	if (cmd->args.empty()) {
+		const char *home = getenv("HOME");
+		if (home == nullptr) {
+			fprintf(stderr, "cd: HOME not set\n");
+			return 1;
+		}
+		if (chdir(home) != 0) {
+			perror("cd");
+			return 1;
+		}
+	} else {
+		if (chdir(cmd->args[0].c_str()) != 0) {
+			perror("cd");
+			return 1;
 		}
 	}
+	return 0;
+}
+
+static bool
+execute_command_line(const struct command_line *line, int &exit_code)
+{
+	assert(line != NULL);
+	
+	if (line->exprs.empty()) {
+		return false;
+	}
+	
+	if (line->exprs.size() == 1 && line->exprs.front().type == EXPR_TYPE_COMMAND) {
+		const struct command *cmd = &line->exprs.front().cmd.value();
+		
+		if (cmd->exe == "cd") {
+			exit_code = execute_cd_command(cmd);
+			return false;
+		}
+		
+		if (cmd->exe == "exit") {
+			if (line->out_type == OUTPUT_TYPE_STDOUT) {
+				exit_code = 0;
+				if (!cmd->args.empty()) {
+					exit_code = atoi(cmd->args[0].c_str());
+				}
+				return true;
+			}
+			return false;
+		}
+		
+		int output_fd = STDOUT_FILENO;
+		if (line->out_type == OUTPUT_TYPE_FILE_NEW) {
+			output_fd = open(line->out_file.c_str(), O_CREAT | O_WRONLY | O_TRUNC, 0644);
+			if (output_fd < 0) {
+				perror("open");
+				return false;
+			}
+		} else if (line->out_type == OUTPUT_TYPE_FILE_APPEND) {
+			output_fd = open(line->out_file.c_str(), O_CREAT | O_WRONLY | O_APPEND, 0644);
+			if (output_fd < 0) {
+				perror("open");
+				return false;
+			}
+		}
+		
+		int result = execute_single_command(cmd, STDIN_FILENO, output_fd);
+		exit_code = result;
+		
+		if (output_fd != STDOUT_FILENO) {
+			close(output_fd);
+		}
+		return false;
+	}
+	std::vector<struct command> commands;
+	std::vector<int> pipe_fds;
+	
+	for (const auto &e : line->exprs) {
+		if (e.type == EXPR_TYPE_COMMAND) {
+			commands.push_back(e.cmd.value());
+		}
+	}
+	
+	if (commands.empty()) {
+		return false;
+	}
+	
+	for (size_t i = 0; i < commands.size() - 1; ++i) {
+		int pipefd[2];
+		if (pipe(pipefd) == -1) {
+			perror("pipe");
+			for (auto &fd : pipe_fds) {
+				close(fd);
+			}
+			return false;
+		}
+		pipe_fds.push_back(pipefd[0]);
+		pipe_fds.push_back(pipefd[1]);
+	}
+	std::vector<pid_t> pids;
+	for (size_t i = 0; i < commands.size(); ++i) {
+		int input_fd = (i == 0) ? STDIN_FILENO : pipe_fds[(i - 1) * 2];
+		int output_fd = STDOUT_FILENO;
+		
+		if (i < commands.size() - 1) {
+			output_fd = pipe_fds[i * 2 + 1];
+		}
+		
+		if (i == commands.size() - 1) {
+			if (line->out_type == OUTPUT_TYPE_FILE_NEW) {
+				output_fd = open(line->out_file.c_str(), O_CREAT | O_WRONLY | O_TRUNC, 0644);
+				if (output_fd < 0) {
+					perror("open");
+					for (auto &fd : pipe_fds) {
+						close(fd);
+					}
+					return false;
+				}
+			} else if (line->out_type == OUTPUT_TYPE_FILE_APPEND) {
+				output_fd = open(line->out_file.c_str(), O_CREAT | O_WRONLY | O_APPEND, 0644);
+				if (output_fd < 0) {
+					perror("open");
+					for (auto &fd : pipe_fds) {
+						close(fd);
+					}
+					return false;
+				}
+			}
+		}
+		
+		if (commands[i].exe == "cd") {
+			if (i == 0) {
+				exit_code = execute_cd_command(&commands[i]);
+			}
+			for (auto &fd : pipe_fds) {
+				close(fd);
+			}
+			return false;
+		}
+		
+		if (commands[i].exe == "exit") {
+			int exit_code_local = 0;
+			if (!commands[i].args.empty()) {
+				exit_code_local = atoi(commands[i].args[0].c_str());
+			}
+			
+			pid_t pid = fork();
+			if (pid == 0) {
+				if (input_fd != STDIN_FILENO) {
+					dup2(input_fd, STDIN_FILENO);
+					close(input_fd);
+				}
+				if (output_fd != STDOUT_FILENO) {
+					dup2(output_fd, STDOUT_FILENO);
+					close(output_fd);
+				}
+				
+				for (auto &fd : pipe_fds) {
+					close(fd);
+				}
+				
+				exit(exit_code_local);
+			} else if (pid > 0) {
+				pids.push_back(pid);
+			} else {
+				perror("fork");
+				for (auto &fd : pipe_fds) {
+					close(fd);
+				}
+				return false;
+			}
+			continue;
+		}
+		
+		pid_t pid = fork();
+		if (pid == 0) {
+			if (input_fd != STDIN_FILENO) {
+				dup2(input_fd, STDIN_FILENO);
+				close(input_fd);
+			}
+			if (output_fd != STDOUT_FILENO) {
+				dup2(output_fd, STDOUT_FILENO);
+				close(output_fd);
+			}
+			
+			for (auto &fd : pipe_fds) {
+				close(fd);
+			}
+			
+			char **argv = new char*[commands[i].args.size() + 2];
+			argv[0] = strdup(commands[i].exe.c_str());
+			for (size_t j = 0; j < commands[i].args.size(); ++j) {
+				argv[j + 1] = strdup(commands[i].args[j].c_str());
+			}
+			argv[commands[i].args.size() + 1] = nullptr;
+			
+			execvp(commands[i].exe.c_str(), argv);
+			
+			perror("execvp");
+			
+			for (size_t j = 0; j <= commands[i].args.size(); ++j) {
+				free(argv[j]);
+			}
+			delete[] argv;
+			
+			exit(EXIT_FAILURE);
+		} else if (pid > 0) {
+			pids.push_back(pid);
+		} else {
+			perror("fork");
+			for (auto &fd : pipe_fds) {
+				close(fd);
+			}
+			return false;
+		}
+	}
+	
+	for (auto &fd : pipe_fds) {
+		close(fd);
+	}
+	
+	int last_status = 0;
+	for (auto pid : pids) {
+		int status;
+		waitpid(pid, &status, 0);
+		last_status = status;
+	}
+	
+	if (WIFEXITED(last_status)) {
+		exit_code = WEXITSTATUS(last_status);
+	}
+	
+	if (commands.size() == 1 && commands[0].exe == "exit" &&
+		line->out_type == OUTPUT_TYPE_STDOUT) {
+		exit_code = 0;
+		if (!commands[0].args.empty()) {
+			exit_code = atoi(commands[0].args[0].c_str());
+		}
+		return true;
+	}
+	
+	return false;
 }
 
 int
@@ -48,8 +307,11 @@ main(void)
 	const size_t buf_size = 1024;
 	char buf[buf_size];
 	int rc;
+	int exit_code = 0;
+	bool should_exit = false;
 	struct parser *p = parser_new();
-	while ((rc = read(STDIN_FILENO, buf, buf_size)) > 0) {
+	
+	while (!should_exit && (rc = read(STDIN_FILENO, buf, buf_size)) > 0) {
 		parser_feed(p, buf, rc);
 		struct command_line *line = NULL;
 		while (true) {
@@ -57,13 +319,14 @@ main(void)
 			if (err == PARSER_ERR_NONE && line == NULL)
 				break;
 			if (err != PARSER_ERR_NONE) {
-				printf("Error: %d\n", (int)err);
+				fprintf(stderr, "Parser error: %d\n", (int)err);
 				continue;
 			}
-			execute_command_line(line);
+			should_exit = execute_command_line(line, exit_code);
 			delete line;
 		}
 	}
+	
 	parser_delete(p);
-	return 0;
+	return exit_code;
 }
